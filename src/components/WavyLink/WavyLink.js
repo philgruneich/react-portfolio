24b505029ee9bf3af06970874b601f082964b9d0
Phil Gruneich <github@onetapless.com>
2023-05-13 09:31:59 -0400

    fix: removes slow wavy background because styled-components is bugged

--- src/components/WavyLink/WavyLink.js
+++ src/components/WavyLink/WavyLink.js
@@ -2,8 +2,7 @@ import styled from 'styled-components';
 
 const wavyBackground = `data:image/svg+xml;,${encodeURIComponent("<svg viewBox='0 0 20 6' xmlns='http://www.w3.org/2000/svg'><path fill='none' d='M0 0h20v6H0z'/><path d='M0 4.782c5 0 5-3.582 10-3.582s5 3.582 10 3.582S25 1.2 30 1.2s5 3.582 10 3.582' class='q' fill='none' stroke='#fff' stroke-width='1.8'/></svg>")}`;
 
-const wavyBackgroundHover = `data:image/svg+xml;,${encodeURIComponent("<svg viewBox='0 0 20 6' xmlns='http://www.w3.org/2000/svg'><style>.q{animation:s .2s linear infinite;}@keyframes s {from {transform:translateX(0);}to {transform:translateX(-20px);}}</style><path fill='none' d='M0 0h20v6H0z'/><path d='M0 4.782c5 0 5-3.582 10-3.582s5 3.582 10 3.582S25 1.2 30 1.2s5 3.582 10 3.582' class='q' fill='none' stroke='#fff' stroke-width='1.8'/></svg>")}`;
-const wavyBackgroundSlow = `data:image/svg+xml;,${encodeURIComponent("<svg viewBox='0 0 20 6' xmlns='http://www.w3.org/2000/svg'><style>.q{animation:s .8s linear infinite;}@keyframes s {from {transform:translateX(0);}to {transform:translateX(-20px);}}</style><path fill='none' d='M0 0h20v6H0z'/><path d='M0 4.782c5 0 5-3.582 10-3.582s5 3.582 10 3.582S25 1.2 30 1.2s5 3.582 10 3.582' class='q' fill='none' stroke='#fff' stroke-width='1.8'/></svg>")}`;
+const wavyBackgroundHover = `data:image/svg+xml;,${encodeURIComponent("<svg viewBox='0 0 20 6' xmlns='http://www.w3.org/2000/svg'><style>.k{animation:s .2s linear infinite;}@keyframes s {from {transform:translateX(0);}to {transform:translateX(-20px);}}</style><path fill='none' d='M0 0h20v6H0z'/><path d='M0 4.782c5 0 5-3.582 10-3.582s5 3.582 10 3.582S25 1.2 30 1.2s5 3.582 10 3.582' class='k' fill='none' stroke='#fff' stroke-width='1.8'/></svg>")}`;
 
 export const WavyLink = styled.a`
   position: relative;
@@ -16,15 +15,11 @@ export const WavyLink = styled.a`
     left: 0;
     width: 100%;
     height: 100%;
-    background-image: url("${wavyBackgroundSlow}");
+    background-image: url("${wavyBackground}");
     background-position: bottom;
     background-repeat: repeat-x;
     background-size: 20%;
     content: "";
-
-    @media (min-width: 992px) {
-      background-image: url("${wavyBackground}");
-    }
   }
 
   &:hover {
