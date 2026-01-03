8ebfefdf479ad6b9b2df90d0124d3bcccd6709bb
Phil Gruneich <github@onetapless.com>
2026-01-03 16:54:13 -0500

    patch: change export config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
 
 module.exports = {
+  output: 'export',
   webpack: config => {
     config.resolve.alias['~'] = path.resolve(path.join(__dirname, 'src'));
     config.resolve.plugins = [new DirectoryNamedWebpackPlugin(true)];
